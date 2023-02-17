fcbd525701788eece147dc7becf7838173f0e9f7
Leyla Shafiyeva <llshafi03@gmail.com>
2023-02-17 15:09:27 +0400

    Add project files.

---
+++ SysProgHW2/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace SysProgHW2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.From = new System.Windows.Forms.Label();
+            this.To = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnFileFrom = new System.Windows.Forms.Button();
+            this.btnFileTo = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnSuspend = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.btnAbort = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // From
+            // 
+            this.From.AutoSize = true;
+            this.From.Location = new System.Drawing.Point(107, 51);
+            this.From.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.From.Name = "From";
+            this.From.Size = new System.Drawing.Size(50, 20);
+            this.From.TabIndex = 0;
+            this.From.Text = "From";
+            // 
+            // To
+            // 
+            this.To.AutoSize = true;
+            this.To.Location = new System.Drawing.Point(107, 155);
+            this.To.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.To.Name = "To";
+            this.To.Size = new System.Drawing.Size(29, 20);
+            this.To.TabIndex = 1;
+            this.To.Text = "To";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(261, 48);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(273, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(261, 152);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(273, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btnFileFrom
+            // 
+            this.btnFileFrom.Location = new System.Drawing.Point(566, 44);
+            this.btnFileFrom.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnFileFrom.Name = "btnFileFrom";
+            this.btnFileFrom.Size = new System.Drawing.Size(124, 35);
+            this.btnFileFrom.TabIndex = 5;
+            this.btnFileFrom.Text = "File...";
+            this.btnFileFrom.UseVisualStyleBackColor = true;
+            this.btnFileFrom.Click += new System.EventHandler(this.btnFileFrom_Click);
+            // 
+            // btnFileTo
+            // 
+            this.btnFileTo.Location = new System.Drawing.Point(566, 152);
+            this.btnFileTo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnFileTo.Name = "btnFileTo";
+            this.btnFileTo.Size = new System.Drawing.Size(124, 35);
+            this.btnFileTo.TabIndex = 6;
+            this.btnFileTo.Text = "File...";
+            this.btnFileTo.UseVisualStyleBackColor = true;
+            this.btnFileTo.Click += new System.EventHandler(this.btnFileTo_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(111, 262);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(267, 35);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // btnSuspend
+            // 
+            this.btnSuspend.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuspend.Location = new System.Drawing.Point(386, 262);
+            this.btnSuspend.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSuspend.Name = "btnSuspend";
+            this.btnSuspend.Size = new System.Drawing.Size(70, 35);
+            this.btnSuspend.TabIndex = 8;
+            this.btnSuspend.Text = "Suspend";
+            this.btnSuspend.UseVisualStyleBackColor = true;
+            // 
+            // btnResume
+            // 
+            this.btnResume.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResume.Location = new System.Drawing.Point(464, 262);
+            this.btnResume.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(70, 35);
+            this.btnResume.TabIndex = 9;
+            this.btnResume.Text = "Resume";
+            this.btnResume.UseVisualStyleBackColor = true;
+            // 
+            // btnAbort
+            // 
+            this.btnAbort.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbort.Location = new System.Drawing.Point(542, 262);
+            this.btnAbort.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAbort.Name = "btnAbort";
+            this.btnAbort.Size = new System.Drawing.Size(70, 35);
+            this.btnAbort.TabIndex = 10;
+            this.btnAbort.Text = "Abort";
+            this.btnAbort.UseVisualStyleBackColor = true;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(620, 262);
+            this.btnCopy.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(70, 35);
+            this.btnCopy.TabIndex = 11;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 342);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnAbort);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnSuspend);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnFileTo);
+            this.Controls.Add(this.btnFileFrom);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.To);
+            this.Controls.Add(this.From);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label From;
+        private System.Windows.Forms.Label To;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnFileFrom;
+        private System.Windows.Forms.Button btnFileTo;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnSuspend;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.Button btnAbort;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
---
+++ SysProgHW2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SysProgHW2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFileFrom_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnFileTo_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
